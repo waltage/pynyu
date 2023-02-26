c9af86e9fdecf1e62e376a7981aa15a0edd1f2c0
Daniel Walt <daniel.r.walt@gmail.com>
2023-02-26 14:13:25 -0500

    code formatting configs

---
+++ src/pynyu/hello.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+
+def hello_world():
+  print("hello")
+
+
+def next():
+  print("wee")
+
+
+#  here is a co
+# mmen
--- start_venv.sh
+++ start_venv.sh
@@ -2,5 +2,7 @@
 
 # starts an in-tree virtual environment
 python3 -m venv .venv
-
+source .venv/bin/activate
+pip3 install requirements.txt
+pre-commit install
 echo "run 'source .venv/bin/activate'"
