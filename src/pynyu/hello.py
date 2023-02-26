7ff6652924e9ce2b552a1c7709ba59b8c25514b1
Daniel Walt <daniel.r.walt@gmail.com>
2023-02-26 14:49:49 -0500

    finish auto formatting settings and precommit

--- src/pynyu/hello.py
+++ src/pynyu/hello.py
@@ -2,12 +2,4 @@ from __future__ import annotations
 
 
 def hello_world():
-  print("hello")
-
-
-def next():
-  print("wee")
-
-
-#  here is a co
-# mmen
+    print("hello")
--- start_venv.sh
+++ start_venv.sh
@@ -3,6 +3,6 @@
 # starts an in-tree virtual environment
 python3 -m venv .venv
 source .venv/bin/activate
-pip3 install requirements.txt
+pip3 install -r requirements.txt
 pre-commit install
 echo "run 'source .venv/bin/activate'"
