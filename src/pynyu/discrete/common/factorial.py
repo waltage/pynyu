d76fd511aec9f77d65358c7d2d33c116aa09934c
Daniel Walt <daniel.r.walt@gmail.com>
2023-02-26 17:29:38 -0500

    refactor multiply sequence to separate file + tests

--- src/pynyu/discrete/common/factorial.py
+++ src/pynyu/discrete/common/factorial.py
@@ -2,30 +2,7 @@ from __future__ import annotations
 
 from copy import deepcopy
 
-
-class MultiplySequence(object):
-    def __init__(self, low: int, high: int):
-        if low < 1:
-            raise ValueError("sequence must be over positive integers")
-        if high < low:
-            raise ValueError("sequence must be monotonic increasing")
-        if not isinstance(low, int) or not isinstance(high, int):
-            raise TypeError("sequence must be over integers")
-        self.low = low
-        self.high = high
-
-    @property
-    def value(self) -> int:
-        result = 1
-        for i in range(self.low, self.high + 1):
-            result *= i
-        return result
-
-    def __str__(self) -> str:
-        if self.low <= 2:
-            return f"{self.high}!"
-        else:
-            return " * ".join([f"{i}" for i in range(self.high, self.low - 1, -1)])
+from pynyu.discrete.common.multiply_sequence import MultiplySequence
 
 
 class Factorial(object):
--- src/pynyu/discrete/common/test_factorial.py
+++ src/pynyu/discrete/common/test_factorial.py
@@ -1,43 +1,7 @@
 import math
-from pynyu.discrete.common.factorial import MultiplySequence
+
 from pynyu.discrete.common.factorial import Factorial
-import pytest
-
-
-def test_bad_sequence_non_positive():
-    with pytest.raises(ValueError):
-        MultiplySequence(-1, 100)
-
-
-def test_bad_sequence_non_monotonic_increasing():
-    with pytest.raises(ValueError):
-        MultiplySequence(10, 4)
-
-
-def test_bad_sequence_types():
-    with pytest.raises(TypeError):
-        MultiplySequence(1.2, 4)
-
-    with pytest.raises(TypeError):
-        MultiplySequence(1, 20.3)
-
-
-def test_sequence_str_true_factorial():
-    seq = MultiplySequence(1, 10)
-    assert str(seq) == "10!"
-
-
-def test_sequence_str_mult_sequence():
-    seq = MultiplySequence(3, 10)
-    assert str(seq) == "10 * 9 * 8 * 7 * 6 * 5 * 4 * 3"
-
-
-def test_sequence_value():
-    seq = MultiplySequence(1, 5)
-    assert seq.value == 5 * 4 * 3 * 2 * 1
-
-    seq = MultiplySequence(2, 4)
-    assert seq.value == 4 * 3 * 2
+from pynyu.discrete.common.multiply_sequence import MultiplySequence
 
 
 def test_factorial_basic():
